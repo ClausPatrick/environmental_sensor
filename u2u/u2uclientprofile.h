f0b41694b0d2f18aef716fc370676b7b3b0c4a4f
ClausPatrick <44665589+ClausPatrick@users.noreply.github.com>
2026-05-18 13:02:21 +0100

    Create u2uclientprofile.h

---
+++ u2u/u2uclientprofile.h
@@ -0,0 +1,25 @@
+//u2uclientprofile.h
+#ifndef U2UCLIENTPROFILE_H
+#define U2UCLIENTPROFILE_H
+
+
+#define U2U_PLATFORM_CHANNELS 2
+#define SELF_NAME  "BLOODHOUND"
+
+
+#define UART_RX_IN 13
+#define UART_TX_IN 12
+#define UART_RX_OUT 5
+#define UART_TX_OUT 4
+
+#define LED_R 11
+#define LED_Y 14
+#define LED_G 15
+
+
+//#define TOPIC_AMOUNT 16
+
+
+extern const char* topic_default_responses[];
+
+#endif //U2UCLIENTPROFILE_H
